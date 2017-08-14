6f9da2b872ecf9a88effd44ecec51b9c924c8022
ShineLee <shine@vip.qq.com>
2017-08-14 15:17:43 +0800

    增加动态UA

--- webspider/spiders/a51CTOBookSpider.py
+++ webspider/spiders/a51CTOBookSpider.py
@@ -10,9 +10,7 @@ class A51ctobookspiderSpider(scrapy.Spider):
     headers = {
         'Accept': 'text / html, application / xhtml + xml, application / xml;q = 0.9, image / webp, image / apng, * / *;q = 0.8',
         'Accept-Encoding': 'deflate, br',
-        'Conection': 'keep-alive',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
-
+        'Conection': 'keep-alive'
     }
 
     def start_requests(self):
